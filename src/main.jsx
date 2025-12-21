9c8e698acf91517c0d562b1d33fd0efecce01ffe
ahoora <ahooravhp@gmail.com>
2025-12-22 03:14:49 +0330

    Wrap App component with HashRouter for routing support

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // <--- 1. Import this
 import './index.css'
 import App from './App.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter> 
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
